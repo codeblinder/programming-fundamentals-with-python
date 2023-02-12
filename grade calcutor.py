d3eb8a2d1e41c907bcbbb74169b37f770baa3472
codeblinder <125188543+codeblinder@users.noreply.github.com>
2023-02-12 16:01:51 +0500

    from grade calculator you can calculate you grade

---
+++ grade calcutor.py
@@ -0,0 +1,17 @@
+
+marks = int(input("enter your marks"))
+if marks <=40:
+    print("you got an F")
+elif marks <=50:
+    print("you got a  E")
+elif marks <=60:
+    print("you got a  D")
+elif marks <=70:
+    print("you got a  C")
+elif marks <=80:
+    print("you got a  B")
+elif marks <=90:
+    print("you got an A")
+elif marks <=100:
+    print("you got an A*")
+
